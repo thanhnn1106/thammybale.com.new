19f6b5cdb01112551841c0b6d88a159e6de38039
ngthanh <thanh.nn1106@gmail.com>
2017-12-17 17:02:41 +0700

    Fix logo hone

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -33,7 +33,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-4 col-md-3 col-sm-6 col-xs-12">
-                        <a href="index.html"><img src="{{ asset('images/bale/logo.png') }}" alt=""></a>
+                        <a href="/"><img src="{{ asset('images/bale/logo.png') }}" alt=""></a>
                     </div>
                     <div class="col-lg-6 col-md-9 col-sm-12 col-xs-12">
                         <div class="navigation-wrapper">
